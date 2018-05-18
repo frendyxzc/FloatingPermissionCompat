04dd1f48849c1bbb8e74ccf277a2ab6a35afe7bd
yingjia.li <yingjia.li@cootek.cn>
2018-05-18 14:15:17 +0800

    修改OPPO5.1浮窗权限不兼容问题

--- library/src/main/java/com/linchaolong/android/floatingpermissioncompat/impl/OppoCompatImpl.java
+++ library/src/main/java/com/linchaolong/android/floatingpermissioncompat/impl/OppoCompatImpl.java
@@ -18,7 +18,7 @@ public class OppoCompatImpl extends BelowApi23CompatImpl {
     public boolean apply(Context context) {
         try {
             Intent intent = new Intent(Intent.ACTION_MAIN);
-            ComponentName componentName = new ComponentName("com.oppo.safe", "com.oppo.safe.permission.PermissionTopActivity");
+            ComponentName componentName = new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.permission.floatwindow.FloatWindowListActivity");
             intent.setComponent(componentName);
             context.startActivity(intent);
             return true;
